6a26444d71126ef86f2251efd00224d6b256cedc
HakimHC <ahmedh90111@gmail.com>
2024-02-29 22:03:00 +0100

    Main program logic updated

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 
 #include "TintinReporter.hpp"
+#include "SignalHandler.hpp"
 #include "Server.hpp"
 #include "Daemonizer.hpp"
 #include "FileHandler.hpp"
@@ -22,30 +23,63 @@ int main() {
 
 
     if (FileHandler::createDirectory(LOG_DIR_PATH) == -1) {
-        perror("mkdir");
         std::cerr << "fatal: cannot create directory" << std::endl;
+        perror("mkdir");
         return 1;
     }
 
-
+    #if 1
     TintinReporter::setLogFile(
         FileHandler::openFileOutput(LOG_FILE_PATH)
     );
+    #else
+    TintinReporter::setLogFile(
+        FileHandler::openFileOutput("/dev/stdout")
+    );
+    #endif
+
+
     TintinReporter::info("Started.");
 
-    Daemonizer::daemonize();
+
+    try {
+        TintinReporter::info("Locking matt_daemon.lock file.");
+        FileHandler::lock(LOCK_FILE_PATH);
+    }
+    catch (std::exception& e) {
+        std::string errMessage = "matt_daemon.lock is already locked. Shutting down...";
+        TintinReporter::error(errMessage);
+        std::cerr << errMessage << std::endl;
+        delete TintinReporter::getLogFile();
+        return 1;
+    }
+
+    SignalHandler::sigListen();
+    int exitCode = 0;
+
+    try {
+        Daemonizer::daemonize();
+    } catch (std::exception& e) {
+        TintinReporter::error(e.what());
+        exitCode = 1;
+        goto handle;
+    }
+
+    SignalHandler::sigListen();
 
     try {
         Server server;
+        server.run();
     }
     catch (std::exception &e) {
         TintinReporter::error(e.what());
         TintinReporter::error("Shutting down...");
-        // TODO: clean up (unlock files, free memory etc)
-        std::exit(1);
+        exitCode =  1;
     }
 
-    while (true) {
-        usleep(400);
-    }
+handle:
+    FileHandler::unlock();
+    delete TintinReporter::getLogFile();
+
+    return exitCode;
 }
